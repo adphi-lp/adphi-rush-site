2139ee459480713523306cac0ccef98dd7b840f7
Jeffrey <jdshen@mit.edu>
2012-12-30 17:56:24 -0500

    Very very minor edits.

--- RushWebsite/server.js
+++ RushWebsite/server.js
@@ -6,10 +6,12 @@ app.set('views', __dirname + '/views');
 //set path to static files
 app.use(express.static(__dirname + '/../public'));
 //handle GET requests on /
-app.get('/vote', function(req, res){res.render('vote.jade',
-	{title: 'Voting', brother: 'JS', rushee: 'Shike', phone: 'unknown', sponsor: null});});
-app.post('/vote', function(req, res){res.render('vote.jade',
-	{title: 'Voting', brother: 'JS', rushee: 'Shike', phone: 'unknown', sponsor: null});});
+
+
+app.get('/vote', function(req, res){
+	res.render('vote.jade',{brother: 'JS', rushee: 'rushee', phone: 'unknown', sponsor: ['JS', 'JS2']});});
+app.post('/vote', function(req, res){
+	res.render('vote.jade',{brother: 'JS', rushee: 'rushee', phone: 'unknown', sponsor: ['JS', 'JS2']});});
 app.get('/', function(req, res){res.render('index.jade', {title: 'Rush home'});});
 //listen on localhost:3000
 app.listen(3000);
