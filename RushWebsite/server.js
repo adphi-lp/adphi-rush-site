829befc8c33e421edfc9500c6ad9fbf2013a60fc
unknown <Jeffrey@.(none)>
2013-01-13 02:56:34 -0500

    Wrote some initial css stuff, some initial work on handling images.

--- RushWebsite/server.js
+++ RushWebsite/server.js
@@ -1,15 +1,19 @@
 //create an app server
 var express = require('express');
+var fs = require('fs');
 var app = express();
 //set path to the views (template) directory
 app.set('views', __dirname + '/views');
 //set path to static files
-app.use(express.static(__dirname + '/../public'));
-//handle GET requests on /
+app.use('/public', express.static(__dirname + '/../public'));
+//set path to static images
+app.use('/img',express.static(__dirname + '/img'))
+app.use('/css',express.static(__dirname + '/css'))
 
 
 app.get('/vote', function(req, res){
 	res.render('vote.jade',{brother: 'JS', rushee: 'rushee', phone: 'unknown', sponsor: ['JS', 'JS2']});});
+
 app.post('/vote', function(req, res){
 	res.render('vote.jade',{brother: 'JS', rushee: 'rushee', phone: 'unknown', sponsor: ['JS', 'JS2']});});
 app.get('/', function(req, res){res.render('index.jade', {title: 'Rush home'});});
